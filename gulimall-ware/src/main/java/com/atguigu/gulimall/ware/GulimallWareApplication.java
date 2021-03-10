d3de676268ef336383e45149b2ad961498dcf074
jay13655670847 <1347025837@qq.com>
2021-03-10 22:25:38 +0800

    项目结构设计

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(GulimallWareApplication.class, args);
     }
 
